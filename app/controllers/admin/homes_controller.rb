8521f35c41026efb3f8807ac8a753d18009b97be
yoshiyumi <yoshi.tennis4423@gmail.com>
2021-09-24 16:24:04 +0000

    nicname

--- app/controllers/admin/homes_controller.rb
+++ app/controllers/admin/homes_controller.rb
@@ -1,6 +1,6 @@
 class Admin::HomesController < ApplicationController
     def top
         @genres = Genre.all
-        @works = Work.all
+        @works = Work.all.page(params[:page]).per(12)
     end
 end
